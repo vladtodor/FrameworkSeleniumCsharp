69720b96b4e230c18155fbbad1f8e8eb9a0631e7
Todor Vlad Ioan <todorvladioan@yahoo.com>
2018-03-21 12:46:13 +0200

    Framework_v1

---
+++ FrameworkSeleniumCsharp/WebTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace FrameworkSeleniumCsharp
+{
+    [TestFixture]
+    public class WebTests
+    {
+        [Test]
+        public void Login()
+        {
+            IWebDriver driverF = new FirefoxDriver("C:/VisualStudioProjects/FrameworkSeleniumCsharp");
+            driverF.Navigate().GoToUrl("https://www.google.ro");
+            driverF.FindElement(By.Id("lst-ib")).SendKeys("Test");
+            driverF.FindElement(By.XPath("//input[@name='btnK']")).Click();
+            driverF.Close();            
+        }
+    }
+}
